c7197be609738fb3e98ad77a0b8144e3569456c2
Euiseok Kim <ikess0330@gmail.com>
2020-01-23 17:30:59 +0900

    prototype app
    Signed-off-by: Euiseok Kim <ikess0330@gmail.com>

--- hanamusic/screens/HomeScreen.js
+++ hanamusic/screens/HomeScreen.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import ScreenName from '../components/ScreenName'
+import TrackPlayer from 'react-native-track-player';
 
 export default class HomeScreen extends React.Component {
+    componentDidMount() {
+        // Creates the player
+        TrackPlayer.setupPlayer().then(async () => {
+
+            // Adds a track to the queue
+            await TrackPlayer.add({
+                id: 'test',
+                url: require('../genesis_1_10.mp3'),
+                title: '창세기 1-10',
+                artist: '모세',
+                artwork: require('../artist.png')
+            });
+
+            // Starts playing it
+            TrackPlayer.play();
+        });
+    }
+
     static navigationOptions = {
     };
     render() {
